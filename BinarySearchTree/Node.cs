67470b03cce66e882d7c35d5ac33d3299e963a05
Angie <angiealters@ymail.com>
2017-03-08 14:42:56 -0600

    Files added

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int initial)
+        {
+            value = initial;
+            left = null;
+            right = null;
+        }
+
+    }
+}
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BinarySearchTree
+{
+    public class Tree
+    {
+        Node top;
+
+        public Tree()
+        {
+            top = null;
+        }
+        public Tree (int initial)
+        {
+            top = new Node(initial);
+        }
+        public void AddNode(int value)
+        {
+            Node currentNode = top;
+            bool added = false;
+
+            if (top == null)
+            {
+                Node newNode = new Node(value);
+                top = newNode;
+                return;
+            }
+            do
+            {
+                if (value < currentNode.value)
+                {
+                    if (currentNode.left == null)
+                    {
+                        Node newNode = new Node(value);
+                        currentNode.left = newNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.left;
+                    }
+                }
+                if (value >= currentNode.value)
+                {
+                    if (currentNode.right == null)
+                    {
+                        Node newNode = new Node(value);
+                        currentNode.right = newNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+            while (!added);
+        }
+
+    }
+}
